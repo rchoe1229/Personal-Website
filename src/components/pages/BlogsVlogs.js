779999934eec1a7afaabfb074faf973cf19ae7a7
rchoe1229 <ryan.choe@outlook.com>
2021-04-08 12:57:48 -0600

    Added Blog

--- src/components/pages/BlogsVlogs.js
+++ src/components/pages/BlogsVlogs.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import '../../App.css'
-import BlogsVlogs from '../BlogsVlogsCards'
+import BlogsVlogsCards from '../BlogsVlogsCards'
 
 export default function BlogsVlogs() {
   return (
     <div>
       <h1 className='BlogVlog-Title'>Blogs and Vlogs</h1>
-      <BlogsVlogs />
+      <BlogsVlogsCards />
     </div>
   )
 }
