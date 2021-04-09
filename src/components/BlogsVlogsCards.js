094115a6d249009b07ad83b19781d61f8f3e25df
rchoe1229 <ryan.choe@outlook.com>
2021-04-09 11:38:31 -0600

    Attempting redirect, a tag added, but not linking with card... Look into CardItems

--- src/components/BlogsVlogsCards.js
+++ src/components/BlogsVlogsCards.js
@@ -1,24 +1,25 @@
-import CardItem from './CardItem'
+import BlogVlogCardItem from './BlogsVlogsCardItem'
 import './Cards.css'
 import Blog from '../images/blogs.jpg'
 
-function Cards() {
+function BlogsVlogsCards() {
   return (
     <div className='cards'>
       <div className="cards__container">
         <div className="cards__wrapper">
+          <a href="https://chowderchoe.medium.com/algorithms-series-pt-1-648d3597c296">
           <div className="cards__items">
-            <CardItem
+            <BlogVlogCardItem
             src={Blog}
             text='Algorithms Series Pt. 1'
             label='First Blog'
-            href="https://chowderchoe.medium.com/algorithms-series-pt-1-648d3597c296"
             />
           </div>
+          </a>
         </div>
       </div>
     </div>
   )
 }
 
-export default Cards
+export default BlogsVlogsCards
